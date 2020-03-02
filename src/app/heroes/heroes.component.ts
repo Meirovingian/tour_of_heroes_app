1618738ef4be5e9bede46e4d4982d267b2e81d4b
Meirovingian <ageorge60420@gmail.com>
2020-03-02 15:01:33 +0100

    Message component and beginning of router linking

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Observable, of } from 'rxjs';
-
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -17,19 +14,15 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
-  constructor(private heroService: HeroService) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
